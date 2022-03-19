d77fd9b021bb2c92318bcb603732a0fcb97fc096
Nguyễn Hữu Toàn <huutoan2603@gmail.com>
2022-03-19 22:48:47 +0700

    connect mongoDB

---
+++ app/config/database.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(`mongodb+srv://${process.env.MONGOOSE_USER}:${process.env.MONGOOSE_PASSWORD}@yomusic.oz27e.mongodb.net/${process.env.MONGOOSE_DATABASE}`,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            }
+        );
+        console.log("MongoDB connecting .......");
+        
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 
-const express = require("express");
+const express = require('express');
 const app = express();
+
+const connectDB = require('./app/config/database');
+
 
 const PORT = process.env.PORT || 8000;
 
-app.get('/' , (req , res)=>{
-
+app.get('/' , (req , res)=> {
    res.send('hello from simple server :)')
-
 });
+
+connectDB();
+
 
 app.listen(PORT, () => {
     console.log(`Listening at: http://localhost:${PORT}`);
