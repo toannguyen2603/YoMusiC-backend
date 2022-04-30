46d89e840fff7b6f33a15e47f9039c59ba4b79b4
Nguyễn Hữu Toàn <huutoan2603@gmail.com>
2022-05-01 01:25:42 +0700

    new task

--- app/config/database.js
+++ app/config/database.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const mongoose = require('mongoose');
 // await mongoose.connect(`mongodb+srv://${process.env.MONGOOSE_USER}:${process.env.MONGOOSE_PASSWORD}@yomusic.oz27e.mongodb.net/${process.env.MONGOOSE_DATABASE}`,
-// 
+
 const connectDB = async () => {
     try {
         await mongoose.connect(`mongodb+srv://yoMusic:yomusic123456@yomusic.qxzsf.mongodb.net/YoMusic`,
