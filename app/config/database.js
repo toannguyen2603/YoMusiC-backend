810ac25ef34c98dcc7ce13da786846e50227fee1
Nguyễn Hữu Toàn <huutoan2603@gmail.com>
2022-05-01 00:06:23 +0700

    make it better

--- app/config/database.js
+++ app/config/database.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 
 const mongoose = require('mongoose');
 // await mongoose.connect(`mongodb+srv://${process.env.MONGOOSE_USER}:${process.env.MONGOOSE_PASSWORD}@yomusic.oz27e.mongodb.net/${process.env.MONGOOSE_DATABASE}`,
-
+// 
 const connectDB = async () => {
     try {
-        await mongoose.connect(`mongodb+srv://yoMusic:yomusic123456@yomusic.oz27e.mongodb.net/YoMusic`,
+        await mongoose.connect(`mongodb+srv://yoMusic:yomusic123456@yomusic.qxzsf.mongodb.net/YoMusic`,
             {
                 useNewUrlParser: true,
                 useUnifiedTopology: true,
