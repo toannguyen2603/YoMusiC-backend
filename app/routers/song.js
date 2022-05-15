be3b358a59dba0cb5a1a18472eedc9414de7203b
Nguyễn Hữu Toàn <huutoan2603@gmail.com>
2022-05-15 14:02:38 +0700

    fix

--- app/routers/song.js
+++ app/routers/song.js
@@ -8,6 +8,6 @@ router.post("/createSong", createSong);
 router.get("/getAllSong", getAllSong);
 router.get("/getMostLikeSong",getMostLikeSong);
 router.patch("/updateLikeForSong/:id", updateLikeForSong);
-router.get("/searchByKeywords", searchByKeywords);
+router.post("/searchByKeywords", searchByKeywords);
 
 module.exports = router;
